2034aecba50fc6276b7629b5d0ef00e2336649c3
Barcelos <barcelosps41710@gmail.com>
2023-08-16 00:13:19 -0300

    era um <p> que nao pedia no exercise!

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,9 +8,10 @@ type Cityinfo = {
 function Card({ cityInfo }: { cityInfo: Cityinfo }) {
   const { city, country, imageUrl, visited = false } = cityInfo;
   return (
-    <div className="Card-city">
+    <div className="ard-city">
       <img src={ imageUrl } alt={ city } />
-      <h2>{`${city}, ${country}`}</h2>
+      <h2>{city}</h2>
+      <p>{country}</p>
       {visited ? <p>Já Fui!</p> : <p>Não Fui.</p>}
     </div>
   );
